ad3475da950bba1618363b0308611adfde82e648
Truyet Nguyen <minhtruyet@gmail.com>
2016-06-05 01:57:56 +0700

    - Complete tour of heroes angular2 tutorial

---
+++ quickstart/app/dashboard.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router-deprecated';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+@Component({
+    selector: 'my-dashboard',
+    templateUrl: 'app/dashboard.component.html',
+    styleUrls: ['app/dashboard.component.css'],
+})
+export class DashboardComponent implements OnInit {
+    heroes: Hero[] = [];
+    constructor(private router: Router, private heroService: HeroService) { }
+    ngOnInit() {
+        this.heroService.getHeroes()
+            .then(heroes => this.heroes = heroes.slice(1,5));
+    }
+    gotoDetail(hero: Hero){ this.router.navigate(['HeroDetail', { id: hero.id }]);}
+}
--- quickstart/app/hero.service.js
+++ quickstart/app/hero.service.js
@@ -1,4 +1,3 @@
-"use strict";
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -10,11 +9,26 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var mock_heroes_1 = require('./mock-heroes');
+var http_1 = require('@angular/http');
+require('rxjs/add/operator/toPromise');
 var HeroService = (function () {
-    function HeroService() {
+    function HeroService(http) {
+        this.http = http;
+        //Old - function
+        //getHeroes() {
+        //  return Promise.resolve(HEROES);
+        //}
+        this.heroesUrl = 'app/heroes';
     }
     HeroService.prototype.getHeroes = function () {
-        return Promise.resolve(mock_heroes_1.HEROES);
+        return this.http.get(this.heroesUrl)
+            .toPromise()
+            .then(function (response) { return response.json().data; })
+            .catch(this.handleError);
+    };
+    HeroService.prototype.handleError = function (error) {
+        console.error('An error occurred', error);
+        return Promise.reject(error.message || error);
     };
     HeroService.prototype.getHeroesSlowly = function () {
         return new Promise(function (resolve) {
@@ -23,11 +37,51 @@ var HeroService = (function () {
          // 2 seconds
         );
     };
+    HeroService.prototype.getHero = function (id) {
+        return this.getHeroes()
+            .then(function (heroes) { return heroes.filter(function (hero) { return hero.id === id; })[0]; });
+    };
+    // Add new Hero
+    HeroService.prototype.post = function (hero) {
+        var headers = new http_1.Headers({
+            'Content-Type': 'application/json' });
+        return this.http
+            .post(this.heroesUrl, JSON.stringify(hero), { headers: headers })
+            .toPromise()
+            .then(function (res) { return res.json().data; })
+            .catch(this.handleError);
+    };
+    // Update existing Hero
+    HeroService.prototype.put = function (hero) {
+        var headers = new http_1.Headers();
+        headers.append('Content-Type', 'application/json');
+        var url = this.heroesUrl + "/" + hero.id;
+        return this.http
+            .put(url, JSON.stringify(hero), { headers: headers })
+            .toPromise()
+            .then(function () { return hero; })
+            .catch(this.handleError);
+    };
+    HeroService.prototype.delete = function (hero) {
+        var headers = new http_1.Headers();
+        headers.append('Content-Type', 'application/json');
+        var url = this.heroesUrl + "/" + hero.id;
+        return this.http
+            .delete(url, headers)
+            .toPromise()
+            .catch(this.handleError);
+    };
+    HeroService.prototype.save = function (hero) {
+        if (hero.id) {
+            return this.put(hero);
+        }
+        return this.post(hero);
+    };
     HeroService = __decorate([
         core_1.Injectable(), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [http_1.Http])
     ], HeroService);
     return HeroService;
-}());
+})();
 exports.HeroService = HeroService;
 //# sourceMappingURL=hero.service.js.map
